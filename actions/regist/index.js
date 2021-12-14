ca07a237b7e2dcf6cbe42788f4d8d42e0af6057a
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-12-14 08:30:07 +0000

    feat: add json parser

--- actions/regist/index.js
+++ actions/regist/index.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 
-const token = process.argv[2].split('\n').find((v) => v.includes('ey')).trim()
+const token = JSON.parse(process.argv[2]).split('\n').find((v) => v.includes('ey')).trim()
 const secret = process.argv[3]
 
 ;(async () => {
